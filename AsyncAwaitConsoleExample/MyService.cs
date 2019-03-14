182932e98cd64930adbb52bd66a2f928c202aba8
Eduardo Monteiro de Barros <eduardo.mb@amwins.com>
2019-03-14 10:22:47 -0400

    Added AsyncAwaitConsoleExample

---
+++ AsyncAwaitConsoleExample/MyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitConsoleExample
+{
+    public class MyService
+    {
+        public async Task<double> RunTask()
+        {
+            return await AddRandomDelay();
+        }
+
+        private static async Task<double> AddRandomDelay()
+        {
+            var time = new Random().NextDouble() * 3.0;
+            await Console.Out.WriteLineAsync($"RunTask1 delaying {time}s");
+            await Task.Delay(TimeSpan.FromSeconds(time));
+            return time;
+        }
+    }
+}
--- AsyncAwaitConsoleExample/Program.cs
+++ AsyncAwaitConsoleExample/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AsyncAwaitConsoleExample
 {
@@ -6,7 +9,48 @@ namespace AsyncAwaitConsoleExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var n = await GetNumberOfTasksToRun();
+
+            if (n <= 0)
+                return;
+
+            await RunTasks(n);
+            await MainAsync(args);
+        }
+
+        private static async Task RunTasks(int n)
+        {
+            var service = new MyService();
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var tasks = new Task<double>[n];
+            for (int i = 0; i < n; i++)
+                tasks[i] = service.RunTask();
+
+            Task.WaitAll(tasks);
+            sw.Stop();
+
+            await Console.Out.WriteLineAsync(".");
+            await Console.Out.WriteLineAsync($"Sum of tasks delay: {tasks.Sum(t => t.Result)}s");
+            await Console.Out.WriteLineAsync($"{n} tasks ran in {sw.ElapsedMilliseconds / 1000.0}s");
+            await Console.Out.WriteLineAsync(".");
+        }
+
+        static async Task<int> GetNumberOfTasksToRun()
+        {
+            await Console.Out.WriteLineAsync($"Type a positive integer number of tasks to run, or anything else to quit:");
+
+            if (!int.TryParse(Console.ReadLine(), out int i))
+                return 0;
+
+            return i;
         }
     }
 }
